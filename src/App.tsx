8c2514b070892e87855faa5b0988dce69e541031
Andrey Prokhorov <ilikeseik@gmail.com>
2023-01-29 11:34:12 +0300

    usind useState and controlled and uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./components/Accordion";
-import {Rating} from "./components/Rating";
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 import {OnOff} from "./components/OnOff/OnOff";
+import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 function App() {
     return (
@@ -10,17 +12,13 @@ function App() {
             <PageTitle title ={"This is APP component"}/>
             <PageTitle title ={"My Friend"}/>
             Article 1
-            <Rating value = {3}/>
-            <Accordion titleValue={"Menu"}  collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>
+            <UncontrolledAccordion titleValue={"Menu"}  />
+            <UncontrolledAccordion titleValue={"Users"} />
             Article 2
             <Rating value = {0}/>
-            <Rating value = {1}/>
-            <Rating value = {2}/>
-            <Rating value = {3}/>
-            <Rating value = {4}/>
-            <Rating value = {5}/>
-            <OnOff selected = {true}/>
+            <UncontrolledRating/>
+            <OnOff/>
+
         </div>
 
     );
--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,15 +1,45 @@
+import {useState} from "react";
+
 type OnOffType = {
-    selected: boolean
+    // selected: boolean
 }
 export function OnOff(props: OnOffType ) {
-    return (
-        <>
-            <span><button>On</button></span>
-            <span><button>Off</button></span>
-            <span><svg height="100" width="100">
-                <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red"/>
-            </svg></span>
 
-        </>
+    let [on, setOn] = useState(false)
+
+    const onStyle = {
+        width: "30px",
+        height: "30px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: on ? "green" : "white"
+    };
+    const offStyle = {
+        width: "30px",
+        height: "30px",
+        border: "1px solid black",
+        padding: "2px",
+        marginLeft: "5px",
+        display: "inline-block",
+        backgroundColor: on ? "white" : "red"
+    };
+    const IndicatorStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "5px",
+        border: "1px solid black",
+        marginLeft: "5px",
+        display: "inline-block",
+        backgroundColor: on ? "green" : "red"
+    };
+
+    return (
+
+        <div>
+            <div style={onStyle} onClick={()=> {setOn(true)}}>On</div>
+            <div style={offStyle} onClick={()=> {setOn(false)}}>Off</div>
+            <div style={IndicatorStyle}></div>
+        </div>
     )
 }
